779b3b029cb6cfdce509197dbaf166add61f6461
luanafreitas <luanaluciele0@gmail.com>
2021-09-29 21:37:22 -0300

    primeiro commit

---
+++ src/Config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAruxh09hrNrSdJQcZv-RyK62nQQM2uXbg",
+  authDomain: "student-manager-c6abb.firebaseapp.com",
+  projectId: "student-manager-c6abb",
+  storageBucket: "student-manager-c6abb.appspot.com",
+  messagingSenderId: "243189575165",
+  appId: "1:243189575165:web:27d7a0227fc69d9b78fd94"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
